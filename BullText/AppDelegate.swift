60c3c4ded12720993cf2027e556bfb9ae279dc47
izloop <peter@ptip.uk>
2020-04-04 15:35:22 +0200

    Initial Commit

---
+++ BullText/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BullText
+//
+//  Created by Izloop on 4/4/20.
+//  Copyright © 2020 IPL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BullText/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BullText
+//
+//  Created by Izloop on 4/4/20.
+//  Copyright © 2020 IPL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
